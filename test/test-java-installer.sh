cc4aab27414fc18004484c7c8d6e63344738c097
Edward Ost <eost@talend.com>
2017-06-15 22:01:09 -0400

    updated download_local

--- test/test-java-installer.sh
+++ test/test-java-installer.sh
@@ -4,5 +4,5 @@ set -e
 source ../java-installer.sh
 
 #java_installer help
-java_installer download_local /home/eost/shared/java/jdk-8u131-linux-x64.tar.gz
-#java_installer install
+java_installer download_local /home/eost/shared/java
+java_installer install
